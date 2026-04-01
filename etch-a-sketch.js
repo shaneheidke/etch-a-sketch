129b161d9bcebcd82c292c1ad0e1db3f57ed8b5d
shaneheidke <heidkeshane@gmail.com>
2026-04-01 14:50:44 -0700

    Add ability to change grid size through user input
    Add the ability for the user to change the grid size to a specified
    value. The value must be a positive integer between 1 and 100. The
    default grid size is 16x16. They can change this by pressing the
    "Change Grid Size" button at the top of the grid and entering the
    desired grid size. When this is done, the grid is emptied and rebuilt
    using the specified grid size, but staying within the designated 960px
    width of the entire grid, meaning the squares will grow or shrink to
    accomodate.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,28 +1,52 @@
 const body = document.querySelector("body");
 const container = document.createElement("div");
 container.classList.add("container");
+
+let gridSize = 16;
+fillGrid();
+
+const button = document.createElement("button");
+button.textContent = "Change Grid Size";
+button.addEventListener("click", () => {
+    let response = Number(prompt("Please enter the preferred grid size (one number x, grid = x * x)"))
+    while (response > 100 || response < 1 || !Number.isInteger(response)) {
+        response = Number(prompt("Invalid number. Choose a number between 1 and 100."));
+    }
+    gridSize = response;
+    fillGrid();
+});
+body.appendChild(button);
 body.appendChild(container);
 body.style.display = "flex";
-body.style.justifyContent = "center"
+body.style.flexDirection = "column";
+body.style.justifyContent = "center";
+body.style.alignItems = "center";
+body.style.boxSizing = "border-box";
+
 
 function changeColor(e) {
     e.target.style.backgroundColor = "black";
 }
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    row.style.display = "flex";
-    
-    for (let j = 0; j < 16; j++) {
-        const box = document.createElement("div");
-        box.classList.add("box");
-        box.style.border = "1px solid black";
-        box.style.padding = "20px"
-        box.addEventListener("mouseover", (e) => changeColor(e))
-        row.appendChild(box);
+function fillGrid() {
+    container.replaceChildren();
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        row.style.display = "flex";
+        row.style.width = "960px"
+
+        for (let j = 0; j < gridSize; j++) {
+            const box = document.createElement("div");
+            box.classList.add("box");
+            box.style.border = "1px solid black";
+            box.style.flex = "1";
+            box.style.aspectRatio = "1";
+            box.addEventListener("mouseover", (e) => changeColor(e))
+            row.appendChild(box);
+        }
+
+        container.appendChild(row);
+
     }
-
-    container.appendChild(row);
-
 }
